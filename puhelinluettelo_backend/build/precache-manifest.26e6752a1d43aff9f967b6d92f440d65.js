ae47adaa7cfaa7d371e343cb95aad73ada59c7ce
Sanna-Mari Äyrämö <santtu.ayramo@gmail.com>
2020-05-12 19:47:32 +0300

    mongodb:tä käyttävän version tuonti, 2.kerta

---
+++ puhelinluettelo_backend/build/precache-manifest.26e6752a1d43aff9f967b6d92f440d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadcbe3535447536fe5e8d58918d462e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058e68a01a640cd80d5e",
+    "url": "/static/css/main.61501bba.chunk.css"
+  },
+  {
+    "revision": "f96d806b3f55726b8a94",
+    "url": "/static/js/2.40695959.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40695959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058e68a01a640cd80d5e",
+    "url": "/static/js/main.5cc3988a.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);

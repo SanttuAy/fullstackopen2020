c42a59138b5b1189982fff7391dffe9208778de6
Sanna-Mari Äyrämö <santtu.ayramo@gmail.com>
2020-05-13 21:38:46 +0300

    kovakoodatut datat poistettu

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -10,30 +10,6 @@ require('dotenv').config()
 const Person = require('./models/person')
 
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
-
-
 app.get('/', (req, res) => {
   res.send('<h1>Puhelinluettelo</h1>')
 })
@@ -44,7 +20,6 @@ app.get('/api/persons', (req, res) => {
     res.json(result.map(yhteystieto => yhteystieto.toJSON()))
   })
 })
-
 
 
 app.get('/info', (req, res) => {

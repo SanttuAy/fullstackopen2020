b1c820c237e187e8a4b8f85ef2a12f6aa1655058
Sanna-Mari Äyrämö <santtu.ayramo@gmail.com>
2020-09-17 11:03:55 +0300

    5.1 kesken

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService
+      .getAll()
+      .then(initialBlogs =>
+      setBlogs(initialBlogs)
+      )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      console.log(user.token)
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+/*
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newBlog
+      author: ...
+      url:  ...
+    }
+  }*/
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+/*  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )*/
+
+/*  const handleBlogChange = (event) => {
+    setNewBlog(event.target.value)
+  }*/
+
+//  if (user === null) {
+/*    return (
+      <div>
+        <h2>Log in to application</h2>
+
+        {loginForm()}
+      </div>
+    )  
+  }*/
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+
+        {loginForm()}
+      </div>
+    )  
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App

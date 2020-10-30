8521b4ab6bac6eb0689bf2ddd78e9494395801e1
Sanna-Mari Äyrämö <santtu.ayramo@gmail.com>
2020-10-30 17:45:15 +0200

    5.7tehty

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [kaikkiTiedot, setKaikkiTiedot] = useState(false)
+  const [buttonLabel, setButtonLabel] = useState('view')
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const showWhenVisible = { display: kaikkiTiedot ? '' : 'none' } //jos false niin tuodaan esiin
+  const toggleButtonLabel = () => {
+    kaikkiTiedot ? setButtonLabel('view') : setButtonLabel('hide')
+    toggleVisibility()
+  }
+  const toggleVisibility = () => { //vaihtaa näkymän päinvastaiseen
+    setKaikkiTiedot(!kaikkiTiedot)
+  }
+  return(
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button
+        onClick={toggleButtonLabel}>{buttonLabel}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <div>
+          likes: {blog.likes} <button>like</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog

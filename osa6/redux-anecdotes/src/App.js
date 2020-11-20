07b0eeb0f5f82e21805b9f78284dc7bc8898e0e3
Sanna-Mari Äyrämö <santtu.ayramo@gmail.com>
2020-11-20 15:17:15 +0200

    6.3, 6.4

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NewAnecdote from './components/NewAnecdote'
+import Anecdotes from './components/Anecdotes'
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Anecdotes />
+      <h2>create new</h2>
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
